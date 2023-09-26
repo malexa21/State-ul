2aaf7661b6256cced7fb048750b5d568655fc21f
malexa21 <aaliroalexandra@gmail.com>
2023-09-26 18:16:18 +0300

    am creeat aplicatia, insa am cateva buguri

--- State/src/App.jsx
+++ State/src/App.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
 
-function App() {
+//hota
+//[0,5] = verde, [6,12] = mov, [12,15]= portocaliu, [>15]= rosu
 
-  return 
+
+
+function stadiuAer({count}) {
+  if (count <= 5) {
+    return <h1 className={stadiuAer}>Aer curat</h1>;
+  }
+
+  if (count > 5 && count < 12) {
+    return <h1 className={stadiuAer}>Aer inchis</h1>;
+  }
+
+  if (count >= 12 && count < 15) {
+    return <h1 className={stadiuAer}>Aer contaminat</h1>;
+  }
+
+  if(count >=15 && count < 22){
+    return <h1 className={stadiuAer}>Periocol de sufocare</h1>;
+  }
+  
+  return <h1>Pericol de incendiu</h1>
 }
 
 
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+    
+    </div>
+  );
+}
+
+export default App;
